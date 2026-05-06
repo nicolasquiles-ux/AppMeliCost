e706913ecefe4c9c6586f8d34b01b62f409f8c41
nicolasquiles-ux <nicolasquiles@gmail.com>
2026-05-06 11:17:31 -0300

    Update gemini-code-1777924028122.py

--- gemini-code-1777924028122.py
+++ gemini-code-1777924028122.py
@@ -14,8 +14,8 @@ TARIFARIO_ENVIOS = {
 }
 
 TASAS_FINANCIACION = {
-    "1 Pago": 0.0, "3 Pagos (Cuota Simple)": 12.5, 
-    "6 Pagos (Cuota Simple)": 23.8, "9 Pagos": 35.0, "12 Pagos": 45.0
+    "1 Pago": 0.0, "3 Pagos (Cuota Simple)": 7, 
+    "6 Pagos (Cuota Simple)": 10, "9 Pagos": 13.5, "12 Pagos": 18
 }
 
 CLAVE_CORRECTA = "CENTRO_PRO_2026"
